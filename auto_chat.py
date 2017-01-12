4e393c71391da733daa72b8845e965cf1eabd561
IdiosyncraticDragon <lgy807720302@gmail.com>
2017-01-13 01:19:02 +0800

    updates

--- auto_chat.py
+++ auto_chat.py
@@ -67,4 +67,5 @@ if __name__ == "__main__":
    bot.train(
      "chatterbot.corpus.english"
    )
-   app.run(host='0.0.0.0', port=8000, debug=True)
+   #app.run(host='0.0.0.0', port=8000, debug=True)
+   app.run(host='0.0.0.0', port=8000, debug=False)
